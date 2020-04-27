1ea06c956bd1e41ba7a51c7b8244753510ad6f26
Conrad Orta <conradorta@gmail.com>
2020-04-27 17:24:56 -0400

    Implemented Bonus Content

--- server.js
+++ server.js
@@ -4,53 +4,76 @@ const hostName = 'localhost';
 
 http.createServer(function (request, response) {
     if (request.url === '/OK') {
-        console.log('Inbound /OK request being processed...');
+        console.log(`Inbound ${request.url} request being processed...`);
         response.writeHead(200);
+        response.write("<html>", { "Content-Type": "text/html" });
+        response.write("<body>");
+        response.write("<h1>Hello, World!</h1>");
+        response.write("</body>");
+        response.write("</html>");
         response.end();
     }
     else if (request.url === '/Bad-Request') {
-        console.log('');
+        console.log(`Inbound ${request.url} request being processed...`);
         response.writeHead(400);
         response.end();
     }
     else if (request.url === '/Created') {
-        console.log('');
-        response.writeHead(201);
+        console.log(`Inbound ${request.url} request being processed...`);
+        if (request.method === 'POST') {
+            response.writeHead(201);
+        }
+        else {
+            response.writeHead(405,
+                {
+                    'Location': 'http://localhost:3000/MethodNotAllowed'
+                });
+        }
+        response.end();
+    }
+    else if (request.url === '/MethodNotAllowed') {
+        console.log(`Inbound ${request.url} request being processed...`);
+        response.writeHead(405);
         response.end();
     }
     else if (request.url === '/Forbidden') {
-        console.log('');
+        console.log(`Inbound ${request.url} request being processed...`);
         response.writeHead(403);
         response.end();
     }
     else if (request.url === '/Found') {
-        console.log('');
-        response.writeHead(302);
+        console.log(`Inbound ${request.url} request being processed...`);
+        response.writeHead(302,
+            {
+                'Location': 'http://localhost:3000/OK'
+            });
         response.end();
     }
     else if (request.url === '/Gateway-Timeout') {
-        console.log('');
+        console.log(`Inbound ${request.url} request being processed...`);
         response.writeHead(504);
         response.end();
     }
     else if (request.url === '/Internal-Server-Error') {
-        console.log('');
+        console.log(`Inbound ${request.url} request being processed...`);
         response.writeHead(500);
         response.end();
     }
     else if (request.url === '/Moved-Permanently') {
-        console.log('');
-        response.writeHead(301);
+        console.log(`Inbound ${request.url} request being processed...`);
+        response.writeHead(301,
+            {
+                'Location': 'http://localhost:3000/OK'
+            });
         response.end();
     }
     else if (request.url === '/Unauthorized') {
-        console.log('Inbound /Unauthorized request being processed...');
+        console.log(`Inbound ${request.url} request being processed...`);
         response.writeHead(401);
         response.end();
     }
     else {
-        console.log('404 Not Found');
-        response.writeHead(404);
+        console.log(`Inbound ${request.url} request being processed...`);
         response.end();
     }
 }).listen(port, hostName, function () {
